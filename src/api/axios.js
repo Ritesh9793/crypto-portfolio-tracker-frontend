649f925c64b0c8b2368cbb4bc943e36477e6e97a
Ritesh Gupta <65335183+Ritesh9793@users.noreply.github.com>
2026-01-27 23:55:39 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://crypto-portfolio-tracker-backend.onrender.com",
   withCredentials: false
 });
 
@@ -14,3 +14,4 @@ api.interceptors.request.use((config) => {
 });
 
 export default api;
+
